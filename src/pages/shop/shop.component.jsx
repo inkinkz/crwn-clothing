bc947e85d8c77c17ce3a142dd3716d96b80e1c5d
inkinkz <nithiphorn.n@gmail.com>
2020-08-04 19:39:12 +0700

    pulled data from firstore into redux, added withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,30 +6,78 @@ import {
   firestore,
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+
 import { updateCollections } from "../../redux/shop/shop.actions";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    collectionRef.onSnapshot(async (snapshot) => {
+
+    // Promise -> needs remount to get new data
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
+
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/crwn-clothing-5c521/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    // Observable -> live update (onSnapshot)
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+    //   (snapshot) => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     updateCollections(collectionsMap);
+    //     this.setState({ loading: false });
+    //   }
+    // );
   }
+
+  // componentWillUnmount() {
+  //   this.unsubscribeFromSnapshot();
+  // }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
+    console.log(match);
     return (
       <div className="shop-page">
-        <Route exact path={match.path} component={CollectionsOverview}></Route>
+        <Route
+          exact
+          path={match.path}
+          render={
+            (props) => (
+              <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            )
+            //  component={CollectionsOverview}
+          }
+        ></Route>
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          // component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         ></Route>
       </div>
     );
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -11,13 +11,12 @@ export const selectCollections = createSelector(
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   // convert JSON into array
-  (collections) => Object.values(collections)
+  (collections) => (collections ? Object.values(collections) : [])
   // (collections) => Object.keys(collections).map((key) => collections[key])
 );
 
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   )
 );
